5467f3aa9a79f4333c554c8a98eb9a937ff1964e
kimxavi <kimxavi11@gmail.com>
2024-02-23 17:36:56 +0900

    vs코드 터미널 에서 수정

--- Test_01/Algorithm/src/Main.java
+++ Test_01/Algorithm/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("자바파일 첫 번째 수정후 커밋");
         System.out.println("vs코드에서 수정하기");
+        System.out.println("vs코드 터미널에서 수정하기");
     }
 }
