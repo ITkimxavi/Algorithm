f5254ab46d062e4008c487eae8c5d083515706aa
kimxavi <kimxavi11@gmail.com>
2024-02-23 17:35:53 +0900

    VScode update main class

--- Test_01/Algorithm/src/Main.java
+++ Test_01/Algorithm/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("자바파일 첫 번째 수정후 커밋");
+        System.out.println("vs코드에서 수정하기");
     }
 }
