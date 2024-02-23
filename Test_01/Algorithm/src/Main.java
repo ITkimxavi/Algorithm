262c483e4598806114b4b512fabbbcf04a2292f1
kimxavi <kimxavi11@gmail.com>
2024-02-23 17:53:20 +0900

    update mainjava

--- Test_01/Algorithm/src/Main.java
+++ Test_01/Algorithm/src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("자바파일 첫 번째 수정후 커밋");
-        System.out.println("vs코드에서 수정하기");
-        System.out.println("vs코드 터미널에서 수정하기");
+        Scanner scan = new Scanner(System.in);
+        int inputYear = scan.nextInt();
+        System.out.println(inputYear-543);
+
+        scan.close();
     }
 }
