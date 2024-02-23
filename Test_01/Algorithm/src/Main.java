7304ba9e119a6075cd4aed958148b5dd30b68d27
kimxavi <kimxavi11@gmail.com>
2024-02-23 17:34:50 +0900

    main.java update

--- Test_01/Algorithm/src/Main.java
+++ Test_01/Algorithm/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("자바파일 첫 번째 수정후 커밋");
     }
 }
